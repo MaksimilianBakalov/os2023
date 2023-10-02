a4e4da36e3100b95a38c96d7cc8bd5946ebb68f2
Maksimilian Bakalov <xbakalov@stuba.sk>
2023-10-02 16:12:57 +0200

    sleep first version

--- user/sleep.c
+++ user/sleep.c
@@ -4,15 +4,10 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
+  if (argc < 2) {
 
-  for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
-    if(i + 1 < argc){
-      write(1, " ", 1);
-    } else {
-      write(1, "\n", 1);
-    }
+  } else if (argc <= 0) {
+    write(1, "error", 5);
   }
   exit(0);
 }
