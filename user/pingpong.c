d528d4c3fcc00380a9329dac5cc8c283c1efd3ed
Maksimilian Bakalov <xbakalov@stuba.sk>
2023-10-02 17:36:41 +0200

    pingpong creation

---
+++ user/pingpong.c
@@ -0,0 +1,22 @@
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
+
+int main(int argc, char *argv[])
+{
+    int p[2];
+    char buf[100];
+    pipe(p);
+
+    int pid = fork();
+    if (pid == 0) {
+        write(p[1], "ping", 4);
+        printf("%d: received ping\n", getpid());
+    } 
+    else {
+        wait(0);
+        read(p[0], buf, 4);
+        printf("%d: received pong\n", getpid());
+    }
+    exit(0);
+}
